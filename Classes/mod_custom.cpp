f4d41affa25b0cd5b89865f80f192175e45e8464
ZhongHan <attackjz@outlook.com>
2015-03-28 12:59:27 +0800

    屏幕适配问题

--- Classes/mod_custom.cpp
+++ Classes/mod_custom.cpp
@@ -32,16 +32,18 @@ bool ModCustom::init()
 
 void ModCustom::InitTop()
 {
+    auto origin = Director::getInstance()->getVisibleOrigin();
+    
     auto size = Director::getInstance()->getVisibleSize();
     layer_top_ = LayerColor::create(Color4B(244, 245, 255, 255));
     layer_top_->setContentSize(Size(size.width,size.height / 15));
-    layer_top_->setPosition(Vec2(0,size.height / 15 * 14));
+    layer_top_->setPosition(Vec2(origin.x + 0,origin.y + size.height / 15 * 14));
     layer_top_->setCascadeColorEnabled(true);
 //    layer_top_->setColor(Color3B(128,128,128));
     
     auto label_title = Label::createWithSystemFont("美食列表", "Arial", 24);
     label_title->setDimensions(layer_top_->getContentSize().width / 4, layer_top_->getContentSize().height);
-    label_title->setPosition(Vec2(size.width / 2,layer_top_->getPositionY() + label_title->getContentSize().height / 2));
+    label_title->setPosition(Vec2(layer_top_->getPositionX() + size.width / 2, layer_top_->getPositionY() + label_title->getContentSize().height / 2));
     label_title->setTextColor(Color4B(102,102,102,255));
     label_title->setAlignment(cocos2d::TextHAlignment::CENTER);
     label_title->setVerticalAlignment(cocos2d::TextVAlignment::CENTER);
@@ -51,12 +53,12 @@ void ModCustom::InitTop()
     auto scale = layer_top_->getContentSize().height/ button_back->getContentSize().height * 0.8;
     button_back->setScale(scale);
     log("size %f",button_back->getContentSize().width );
-    button_back->setPosition(Vec2(button_back->getContentSize().width / 2 * scale,layer_top_->getPositionY() + layer_top_->getContentSize().height / 2));
+    button_back->setPosition(Vec2(layer_top_->getPositionX() + button_back->getContentSize().width / 2 * scale, layer_top_->getPositionY() + layer_top_->getContentSize().height / 2));
     button_back->addTouchEventListener(CC_CALLBACK_2(ModCustom::ButtonBackCallback, this));
     
     auto button_info = Button::create("info.png");
     button_info->setScale(scale);
-    button_info->setPosition(Vec2(size.width - button_back->getContentSize().width / 2 * scale,layer_top_->getPositionY() + layer_top_->getContentSize().height / 2));
+    button_info->setPosition(Vec2(layer_top_->getPositionX() + size.width - button_back->getContentSize().width / 2 * scale, layer_top_->getPositionY() + layer_top_->getContentSize().height / 2));
     button_info->addTouchEventListener(CC_CALLBACK_2(ModCustom::ButtonInfoCallback, this));
     
     
